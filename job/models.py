0b879be6265f7bd707677ca59eebf2615f54f9c9
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-05-26 02:50:41 +0200

    --- Auto-Git Commit ---

--- job/models.py
+++ job/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+ django model field : 
+    - html widget
+
+'''
+class Job(models.Model):  # table 
+    title = models.CharField(max_length=100)  # column
