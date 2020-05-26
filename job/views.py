a5333aee054316725d1f590be33f3ebe8f366bec
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-05-26 23:49:46 +0200

    --- Auto-Git Commit ---

--- job/views.py
+++ job/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    print(job_list)
+    context = {'jobs' :job_list }
+    return render(request,'job/job_list.html',context)
 
 
 
