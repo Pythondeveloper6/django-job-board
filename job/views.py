df35f9b4577474a5a167d4821b5bb7bdcbe45f72
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-06-10 20:49:01 +0200

    --- Auto-Git Commit ---

--- job/views.py
+++ job/views.py
@@ -7,9 +7,11 @@ def job_list(request):
     job_list = Job.objects.all()
 
 
-    paginator = Paginator(job_list, 25) # Show 25 contacts per page.
+    paginator = Paginator(job_list, 1) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    context = {'jobs' :job_list } # template name
+    context = {'jobs' :page_obj } # template name
     return render(request,'job/job_list.html',context)
 
 
